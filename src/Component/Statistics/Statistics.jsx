caf277357ea97f1cde999f8d0f237a55667e1102
Jakaria425 <jakariahassan425@gmail.com>
2024-11-07 00:58:25 +0600

    fix some bug

--- src/Component/Statistics/Statistics.jsx
+++ src/Component/Statistics/Statistics.jsx
@@ -2,7 +2,8 @@
 const Statistics = () => {
     return (
         <div className="box-border pt-24">
-            <div className="bg-[#1bf74a] rounded-2xl h-[500px]">
+            <div className="bg-[#9538E2] flex justify-center items-center rounded-2xl h-[500px]">
+                <h1 className="text-6xl font-bold text-white">No Data Available</h1>
             </div>
         </div>
     );
